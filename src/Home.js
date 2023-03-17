a3286d99131e3053cbb3f6fda58c0a8562f84826
LaurentArcos <laurent.arcos@gmail.com>
2023-03-17 11:49:05 +0100

    sass installed

--- src/Home.js
+++ src/Home.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <h1 className="site-heading">
+      <span className="site-heading-upper">Fresh Coffee Shop</span>
+      <span className="site-heading-lower">Business Casual</span>
+    </h1>
   )
 }
 
