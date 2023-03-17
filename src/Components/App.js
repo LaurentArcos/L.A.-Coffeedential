f861eede3c2a34d5b2eabe9249758eac733514d0
LaurentArcos <laurent.arcos@gmail.com>
2023-03-17 12:26:29 +0100

    change in folder tree and header footer 404 added to Routes in app.js

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,19 +1,28 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import About from './About'
-import Home from './Home'
-import Products from './Products'
-import Store from './Store'
+import About from '../Components/About'
+import Home from '../Components/Home'
+import Products from '../Components/Products'
+import Store from '../Components/Store'
+import NotFound from '../Components/NotFound'
+import Header from '../Components/Header'
+import Footer from '../Components/Footer'
+
+import '../styles/styles.scss'
+
 
 const App = () => {
   return (
     <BrowserRouter>
+    <Header/>
     <Routes>
       <Route path="/" element={<Home/>}/>
       <Route path="/about" element={<About/>}/>
       <Route path="/products" element={<Products/>}/>
       <Route path="/store" element={<Store/>}/>
+      <Route path="*" element={<NotFound/>} />
     </Routes>
+    <Footer/>
     </BrowserRouter>
   )
 }
