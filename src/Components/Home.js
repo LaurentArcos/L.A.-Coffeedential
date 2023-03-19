b510e844f6551dc59f801eed1e77a678e0410719
LaurentArcos <laurent.arcos@gmail.com>
2023-03-19 10:50:15 +0100

    pictures added in assets folder

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,10 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <h1 className="site-heading">
-      <span className="site-heading-upper">Fresh Coffee Shop</span>
-      <span className="site-heading-lower">Business Casual</span>
-    </h1>
+    <div>Home</div>
   )
 }
 
