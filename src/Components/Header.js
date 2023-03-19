d19d8b48540d13b7df2e0cd77193b6a973c6798e
LaurentArcos <laurent.arcos@gmail.com>
2023-03-19 11:55:45 +0100

    navbar fonctionnelle

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <h1 className="header-title">
-      <span className="header-title-main">L.A. Coffeedential</span>
-      <span className="header-title-slogan">Your new favorite coffee place</span>
-    </h1>
+    <div>
+      <h1 className="header-title">
+        <span className="header-title-main">L.A. Coffeedential</span>
+        <span className="header-title-slogan">Your new favorite coffee place</span>
+      </h1>
+
+      <ul className="NavBar">
+      <NavLink to="/" className="NavBar-Links">
+        <li>Accueil</li>
+      </NavLink>
+      <NavLink to="/about" className="NavBar-Links">
+        <li>A propos</li>
+      </NavLink>
+      <NavLink to="/products" className="NavBar-Links">
+        <li>Nos Produits</li>
+      </NavLink>
+      <NavLink to="/store" className="NavBar-Links">
+        <li>Infos pratiques</li>
+      </NavLink>
+    </ul>
+  </div>
   )
 }
 
