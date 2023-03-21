065da74df25e983cea9b1c2596d11aac67bc4e7c
LaurentArcos <laurent.arcos@gmail.com>
2023-03-21 15:10:36 +0100

    hompage bottom text changed

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -31,8 +31,11 @@ const Home = () => {
             <span className='homepage-bottom-h2-span1'>Ce que nous faisons</span>
             <span className='homepage-bottom-h2-span2'>POUR VOUS</span>
           </h2>
-          <p className='homepage-bottom-p'>
-          Que vous veniez chez nous pour démarrer votre journée ou pour une pause gourmande, nous vous garantissons un service chaleureux et amical, dans une atmosphère accueillante et bienveillante, et par dessus tout, d'excellents produits préparés avec des ingrédients de la plus grande qualité. Si toutefois vous n'êtes pas satisfait.e.s, faîtes-le nous savoir et nous mettrons tout en oeuvre pour continuer à nous améliorer !
+          <p className='homepage-bottom-p1'>
+          Que vous veniez chez nous pour démarrer votre journée, pour une pause gourmande ou un moment de détente entre ami.e.s, nous vous garantissons un service chaleureux et amical, dans une atmosphère accueillante et bienveillante. Mais par dessus tout, nos équipes ont à coeur de vous proposer des produits d'excellence, préparés avec des ingrédients de la plus grande qualité sélectionnés avec soin. Nous cherchons sans cesse à nous améliorer donc n'hésitez pas à nous partager vos remarques et conseils ! 
+          </p>
+          <p className='homepage-bottom-p2'>
+          A bientôt !
           </p>
         </div>
       </div>
