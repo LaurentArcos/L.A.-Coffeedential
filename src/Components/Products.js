9ab63dfff93a0e235c73b9960fdc915074022601
LaurentArcos <laurent.arcos@gmail.com>
2023-03-22 17:58:13 +0100

    page products ok with style and french texts

--- src/Components/Products.js
+++ src/Components/Products.js
@@ -9,23 +9,23 @@ const Products = () => {
     <div className='products'>
       
       <h2 className='products-cafe-titles'>
-        <span className='products-cafe-titles-span1'>Blended to Perfection</span>
-        <span className='products-cafe-titles-span2'>Coffees & Teas</span>
+        <span className='products-cafe-titles-span1'>Préparés à la perfection</span>
+        <span className='products-cafe-titles-span2'>cafés et thés</span>
       </h2>
 
       <img className='products-cafe-image' src={cafe} alt="café"/>
 
-      <p className='products-cafe-p'>We take pride in our work, and it shows. Every time you order a beverage from us, we guarantee that it will be an experience worth having. Whether it's our world famous Venezuelan Cappuccino, a refreshing iced herbal tea, or something as simple as a cup of speciality sourced black coffee, you will be coming back for more.</p>
+      <p className='products-cafe-p'>Nous en sommes fiers et il y a de quoi !  Pour chaque commande nous vous garantissons une expérience inoubliable.  Que ce soit pour notre mondialement célèbre Capuccino du Vénézuela, pour un rafraîchissant thé glacé aux fruits rouges ou pour un simple café noir, vous en redemanderez !</p>
 
 
       <h2 className='products-patisserie-titles'>
-        <span className='products-patisserie-titles-span1'>Delicious Treats, Good Eats</span>
-        <span className='products-patisserie-titles-span2'>Bakery & Kitchen</span>
+        <span className='products-patisserie-titles-span1'>Délicieux et sains</span>
+        <span className='products-patisserie-titles-span2'>sucré ou salé ?</span>
       </h2>
 
       <img className='products-patisserie-image' src={croissant} alt="croissant"/>
 
-      <p className='products-patisserie-p'>Our seasonal menu features delicious snacks, baked goods, and even full meals perfect for breakfast or lunchtime. We source our ingredients from local, oragnic farms whenever possible, alongside premium vendors for specialty goods.</p>
+      <p className='products-patisserie-p'>Nos menus saisonniers proposent, d'incroyables pâtisseries et viennoiseries préparées sur place mais également des snacks délicieux et des plats préparés avec soin tous les repas de la journée. Nous achetons autant que possible nos ingrédients chez des producteurs locaux et éco-responsables mais également chez des fournisseurs reconnus et spécialisés.</p>
 
     </div>
   )
