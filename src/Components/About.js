d843e597146fba4e0a7e86da42b5cf8c3b81c155
LaurentArcos <laurent.arcos@gmail.com>
2023-03-22 16:18:17 +0100

    about page finished

--- src/Components/About.js
+++ src/Components/About.js
@@ -10,11 +10,11 @@ const About = () => {
       
       <div className='about-text'>
         <h2 className='about-text-h2'>
-          <span className='about-text-h2-span1'>Strong Coffee, Strong Roots</span>
-          <span className='about-text-h2-span2'>About Our Cafe</span>
+          <span className='about-text-h2-span1'>aux origines</span>
+          <span className='about-text-h2-span2'>Nous et notre café</span>
         </h2>
-        <p className='about-text-p1'>Founded in 1987 by the Hernandez brothers, our establishment has been serving up rich coffee sourced from artisan farmers in various regions of South and Central America. We are dedicated to travelling the world, finding the best coffee, and bringing back to you here in our cafe.</p>
-        <p className='about-text-p2'>We guarantee that you will fall in lust with our decadent blends the moment you walk inside until you finish your last sip. Join us for your daily routine, an outing with friends, or simply just to enjoy some alone time.</p>
+        <p className='about-text-p1'>Ouvert en 1985, notre établissement a servi du café et des produits de qualité, achetés auprès de producteurs artisanaux et éco-responsables en Amérique Latine et en Afrique mais également auprès de producteurs locaux. Nous nous attachons à parcourir le monde à la recherche du meilleur café et des meilleurs produits pour nos viennoiseries et pâtisseries.</p>
+        <p className='about-text-p2'>Nous garantissons que vous serez séduit.e.s par nos savoureux mélanges de cafés dès la première gorgée. Venez nous rejoindre pour accompagner vos morning routines, vos sorties entre ami.e.s ou simplement profiter d'un bon café et d'une pâtisserie en solitaire.</p>
       </div>
     
     </div>
